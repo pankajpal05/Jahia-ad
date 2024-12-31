ce95bcb4885312123a2b1762432da6787580f1d0
pankajpal05 <pankaj.pal@ayasya.com>
2024-12-31 11:37:15 +0530

    update testimonial

---
+++ src/App.jsx
@@ -0,0 +1,87 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+// import required modules
+import "./App.css";
+import { Keyboard, Pagination, Navigation } from "swiper/modules";
+
+import TestimonialCard from "./components/TestimonialCard";
+
+const App = () => {
+  const testimonials = [
+    {
+      title: "Great Service",
+      description: "The service was excellent and exceeded expectations.",
+      image: "https://picsum.photos/id/237/200/300", // Replace with actual image URL
+    },
+    {
+      title: "Amazing Experience",
+      description: "I had an amazing experience, highly recommended!",
+      image: "https://picsum.photos/id/238/200/300", 
+    },
+    {
+      title: "Amazing Experience",
+      description: "I had an amazing experience, highly recommended!",
+      image: "https://picsum.photos/id/239/200/300", 
+    },
+    {
+      title: "Amazing Experience",
+      description: "I had an amazing experience, highly recommended!",
+      image: "https://picsum.photos/id/259/200/300", 
+    },
+    {
+      title: "Amazing Experience",
+      description: "I had an amazing experience, highly recommended!",
+      image: "https://picsum.photos/id/249/200/300", 
+    },
+    {
+      title: "Amazing Experience",
+      description: "I had an amazing experience, highly recommended!",
+      image: "https://picsum.photos/id/289/200/300", 
+    },
+  ];
+
+  return (
+    <>
+      <p className="font-extrabold my-5">Extrabold font weight</p>
+
+      <Swiper
+        keyboard={{
+          enabled: true,
+        }}
+        breakpoints={{
+          425: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+          },
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+          },
+          1024: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+          },
+        }}
+        navigation={true}
+        modules={[Keyboard, Pagination, Navigation]}
+        className="mySwiper"
+      >
+        {testimonials.map((testimonial, index) => (
+          <SwiperSlide key={index}>
+            <TestimonialCard
+              title={testimonial.title}
+              description={testimonial.description}
+              image={testimonial.image}
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </>
+  );
+};
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,54 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        muted: "#6c757d",
+        primary: "#0d6efd",
+        success: "#198754",
+        info: "#0dcaf0",
+        warning: "#ffc107",
+        danger: "#dc3545",
+        secondary: "#6c757d",
+        dark: "#212529",
+        body: "#000000",
+        light: "#f8f9fa",
+        white: "#ffffff",
+      },
+      fontSize: {
+        xs: "0.75rem", // 12px
+        sm: "0.875rem", // 14px
+        base: "1rem", // 16px
+        lg: "1.125rem", // 18px
+        xl: "1.25rem", // 20px
+        "2xl": "1.5rem", // 24px
+        "3xl": "1.875rem", // 30px
+        "4xl": "2.25rem", // 36px
+        "5xl": "3rem", // 48px
+        "6xl": "3.75rem", // 60px
+      },
+      fontWeight: {
+        hairline: 100,
+        thin: 200,
+        light: 300,
+        normal: 400,
+        medium: 500,
+        semibold: 600,
+        bold: 700,
+        extrabold: 800,
+        black: 900,
+      },
+      spacing: {
+        1: "0.25rem", // 4px
+        2: "0.5rem", // 8px
+        3: "0.75rem", // 12px
+        4: "1rem", // 16px
+        5: "1.25rem", // 20px
+        6: "1.5rem", // 24px
+        8: "2rem", // 32px
+      },
+    },
+  },
+  plugins: [],
+};
