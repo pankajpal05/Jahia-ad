3cb4de47348706b18b5acdab152cf75cfc02a17c
noorul <noorul.aga@ayasya.com>
2025-01-02 15:32:58 +0530

    teaser version-2

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,50 @@
+import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-
-// Import Swiper styles
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
-// import required modules
-import "./App.css";
 import { Keyboard, Pagination, Navigation } from "swiper/modules";
-
 import TestimonialCard from "./components/TestimonialCard";
+import TeaserV2 from "./components/TeaserV2";
 
 const App = () => {
   const testimonials = [
     {
       title: "Great Service",
       description: "The service was excellent and exceeded expectations.",
-      image: "https://picsum.photos/id/237/200/300", // Replace with actual image URL
+      image: "https://picsum.photos/id/237/200/300",
     },
     {
       title: "Amazing Experience",
       description: "I had an amazing experience, highly recommended!",
-      image: "https://picsum.photos/id/238/200/300", 
+      image: "https://picsum.photos/id/238/200/300",
     },
-    {
-      title: "Amazing Experience",
-      description: "I had an amazing experience, highly recommended!",
-      image: "https://picsum.photos/id/239/200/300", 
-    },
-    {
-      title: "Amazing Experience",
-      description: "I had an amazing experience, highly recommended!",
-      image: "https://picsum.photos/id/259/200/300", 
-    },
-    {
-      title: "Amazing Experience",
-      description: "I had an amazing experience, highly recommended!",
-      image: "https://picsum.photos/id/249/200/300", 
-    },
-    {
-      title: "Amazing Experience",
-      description: "I had an amazing experience, highly recommended!",
-      image: "https://picsum.photos/id/289/200/300", 
-    },
+    // More testimonials...
   ];
 
   return (
     <>
+    <div className="lg:flex">
+      <div>
+        <TeaserV2
+        preTitle="Special Offer"
+        title="50% Off on All Products"
+        imgUrl="https://colabrio.ams3.cdn.digitaloceanspaces.com/ohio-stage-demo-23/oh__img135.jpg"
+        callToAction="Shop Now"
+      />
+      </div>
+      <div>
+        <TeaserV2
+        preTitle="Special Offer"
+        title="50% Off on All Products"
+        imgUrl="https://colabrio.ams3.cdn.digitaloceanspaces.com/ohio-stage-demo-23/oh__img135.jpg"
+        callToAction="Shop Now"
+      />
+      </div>
+    </div>
       <p className="font-extrabold my-5">Extrabold font weight</p>
-
       <Swiper
-        keyboard={{
-          enabled: true,
-        }}
+        keyboard={{ enabled: true }}
         breakpoints={{
           425: {
             slidesPerView: 1,
