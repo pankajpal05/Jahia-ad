0ef1954d3bf8475aa7b1aa790f54145f063a3fe6
Abhishek Rajput <abhishek.rajput@ayasya.com>
2025-01-06 16:07:30 +0530

    update teaser and testimonialCard

--- src/components/Teaser.jsx
+++ src/components/Teaser.jsx
@@ -3,57 +3,56 @@ import PropTypes from 'prop-types';
 import CTA from './Cta';
 
 const Teaser = ({ title, description, callToAction }) => {
-    return (
-        <div className="teaser h-screen flex items-center justify-center">
-            <div className="cmp-teaser max-w-5xl mx-auto p-6"
-          data-cmp-data-layer="">
-          <div className="cmp-teaser__content card text-center px-6 py-10">
-            {/* Title */}
-            <h2 className="cmp-teaser__title text-center lg:text-6xl text-3xl text-black font-bold my-5 leading-[3.5rem] mx-auto">
-              {title}
-            </h2>
+  return (
+    <div className="teaser h-screen flex items-center justify-center">
+      <div className="cmp-teaser max-w-5xl mx-auto p-6"
+        data-cmp-data-layer="">
+        <div className="cmp-teaser__content flex flex-col gap-5 card text-center px-6 py-10">
+          {/* Title */}
+          <h2 className="cmp-teaser__title text-center lg:text-4xl text-3xl text-black font-semibold leading-[1.5rem] mx-auto">
+            {title}
+          </h2>
+          {/* Description */}
+          {description && (
+            <p className="cmp-teaser__description leading-[1.5rem] text-gray-700 text-lg lg:text-2xl">
+              {description}
+            </p>
+          )}
 
-            {/* Description */}
-            {description && (
-              <p className="cmp-teaser__description text-gray-700 text-lg lg:text-2xl my-6">
-                {description}
-              </p>
-            )}
-
-            {/* Call to Action */}
-            {callToAction && (
-              <div className="cmp-teaser__action-container mt-6">
-                <CTA 
-                ctaText={callToAction} 
-                ctaLink="#" 
-                alignment="center" 
+          {/* Call to Action */}
+          {callToAction && (
+            <div className="cmp-teaser__action-container">
+              <CTA
+                ctaText={callToAction}
+                ctaLink="#"
+                alignment="center"
                 ctaStyle="primary"
                 size="md"
                 icon="./vite.svg"
                 iconPosition="right"
                 bgColor=""
-                ariaLabel="Shop Now" 
-                />
-              </div>
-            )}
-          </div>
+                ariaLabel="Shop Now"
+              />
+            </div>
+          )}
         </div>
       </div>
-    );
+    </div>
+  );
 };
 
 // Default Props
 Teaser.defaultProps = {
-    title: 'Welcome to the Teaser Component!',
-    description: 'A compelling description goes here',
-    callToAction: 'Get Started',
+  title: 'Welcome to the Teaser Component!',
+  description: 'A compelling description goes here',
+  callToAction: 'Get Started',
 };
 
 // Prop Types
 Teaser.propTypes = {
-    title: PropTypes.string.isRequired,
-    description: PropTypes.string,
-    callToAction: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string,
+  callToAction: PropTypes.string.isRequired,
 };
 
 export default Teaser;
--- src/components/TestimonialCard.jsx
+++ src/components/TestimonialCard.jsx
@@ -1,8 +1,10 @@
+import CTA from "./Cta";
+
 // eslint-disable-next-line react/prop-types
-const TestimonialCard = ({ title, description, image }) => {
+const TestimonialCard = ({ title, description, image, cta = false }) => {
   return (
     <div
-      className="teaser relative w-full sm:w-80 md:w-96 h-64 sm:h-80 md:h-96 rounded-lg overflow-hidden shadow-lg group"
+      className="teaser group relative w-full sm:w-80 md:w-96 h-64 sm:h-80 md:h-96 rounded-lg overflow-hidden shadow-lg group"
       role="region"
       aria-labelledby={`card-title-${title}`}
       aria-describedby={`card-description-${title}`}
@@ -15,22 +17,30 @@ const TestimonialCard = ({ title, description, image }) => {
         />
       </div>
       <div
-        className="absolute inset-0 bg-black bg-opacity-50 transition-opacity duration-300 group-hover:bg-opacity-70"
+        className="absolute inset-0 bg-black bg-opacity-50 transition-opacity duration-700 group-hover:bg-opacity-70"
         aria-hidden="true"
-      ></div>
-      <div className="cmp-teaser__content relative z-10 p-4 md:p-5 flex flex-col justify-end h-full">
+      />
+      <div className="cmp-teaser__content translate-y-32 group-hover:translate-y-0 transition duration-700 relative z-10 p-4 md:p-5 flex flex-col justify-end h-full">
         <h3
           id={`card-title-${title}`}
           className="cmp-teaser__title text-lg sm:text-xl md:text-2xl font-semibold text-white mb-1 sm:mb-2"
         >
           {title}
         </h3>
+        <div>
         <p
           id={`card-description-${title}`}
           className="cmp-teaser__description text-xs sm:text-sm md:text-base text-white opacity-90"
         >
           {description}
         </p>
+        {
+          cta &&
+          <div className="cta-btn mt-4">
+            <CTA ctaText="call to action" />
+          </div>
+        }
+        </div>
       </div>
     </div>
   );
