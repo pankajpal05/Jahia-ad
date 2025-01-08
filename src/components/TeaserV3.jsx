183ffccc193058248d320c3cdfa26905ee09e302
anurag-ayasya <110893741+anurag-ayasya@users.noreply.github.com>
2025-01-08 13:05:16 +0530

    props consistent for image

--- src/components/TeaserV3.jsx
+++ src/components/TeaserV3.jsx
@@ -2,17 +2,17 @@ import React from "react";
 import PropTypes from "prop-types";
 import CTA from "./Cta";
 
-export const TeaserV3 = ({ img, description, altText }) => {
+export const TeaserV3 = ({ image, description, altText }) => {
   return (
     <div 
-      className="Teaser hover:bg-white mb-5 px-5 py-6 relative group " 
+      className="Teaser hover:bg-white mb-5 px-5 py-6 relative group cursor-pointer" 
       tabIndex={0} 
       role="region" 
       aria-label="Teaser"
     >
       <div className="cmp-teaser__image">
         <img 
-          src={img} 
+          src={image} 
           alt={altText} 
           className="max-w-fit m-auto" 
           tabIndex={0} 
@@ -24,23 +24,23 @@ export const TeaserV3 = ({ img, description, altText }) => {
           <p className="text-center">{description}</p>
         </div>
       </div>
-      <div className="cmp-action__container mt-2 flex justify-center opacity-0 -translate-x-[30%] group-hover:opacity-100 group-hover:translate-x-0 transition-all duration-500 ease-in-out">
+      {/* <div className="cmp-action__container mt-2 flex justify-center opacity-0 -translate-x-[30%] group-hover:opacity-100 group-hover:translate-x-0 transition-all duration-500 ease-in-out">
         <CTA ctaText="btn" />
-      </div>
+      </div> */}
     </div>
   );
 };
 
 // Define default props
 TeaserV3.defaultProps = {
-  img: "https://cdn.ranosys.com/wp-content/uploads/2024/05/adobe-magento.webp", // Placeholder image as default
+  image: "https://cdn.ranosys.com/wp-content/uploads/2024/05/adobe-magento.webp", // Placeholder image as default
   description: "Default description for the teaser",
   altText: "Teaser image",
 };
 
 // Define prop types
 TeaserV3.propTypes = {
-  img: PropTypes.string,
+  image: PropTypes.string,
   description: PropTypes.string,
   altText: PropTypes.string, 
 };
