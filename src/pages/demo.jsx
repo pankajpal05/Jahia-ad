621466be0465fa0dca20f95de6161572d83c06d7
anurag-ayasya <110893741+anurag-ayasya@users.noreply.github.com>
2025-01-09 17:24:50 +0530

    imported teaser in demo page

--- src/pages/demo.jsx
+++ src/pages/demo.jsx
@@ -10,9 +10,11 @@ import Header from "../components/Header";
 import TestimonialCard from "../components/TestimonialCard";
 import SectionContainer from "../components/SectionContainer";
 import Hero from "../components/Hero";
-import Footer from "../components/Footer";
-import { TeaserV3 } from "../components/TeaserV3";
 import Heading from "../components/Heading";
+import Footer from "../components/Footer";
+import Teaser from "../components/Teaser";
+import TeaserV2 from "../components/TeaserV2";
+import { TeaserV3 } from "../components/TeaserV3";
 import { CardV4 } from "../components/CardV4";
 import CardV5 from "../components/CardV5";
 const Demo = () => {
